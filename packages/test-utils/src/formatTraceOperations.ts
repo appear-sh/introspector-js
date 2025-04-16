0573cca1658a1b9272dbeaa53e4d6095e1c361d4
Jakub Riedl <email@jakubriedl.cz>
2025-04-16 22:03:57 +1000

    Fix flaky test

--- packages/test-utils/src/formatTraceOperations.ts
+++ packages/test-utils/src/formatTraceOperations.ts
@@ -3,7 +3,15 @@ import stringify from "fast-json-stable-stringify"
 export function formatTraceOperations(operations: any[]) {
   return operations
     .map((op) => {
-      op.request.uri = op.request.uri.replace(/:\d+/, ":PORT")
+      op.request.uri = op.request.uri.replace(/:\d+/, ":PORT") // port is dynamic so we need to censor it
+      if (op.response.body?.contentSchema?.properties?.origin) {
+        // origin has port in it so it's dynamic so we set it to plausible value
+        op.response.body.contentSchema.properties.origin = {
+          maxLength: 12,
+          minLength: 12,
+          type: "string",
+        }
+      }
       return op
     })
     .toSorted((a, b) => stringify(a).localeCompare(stringify(b)))
