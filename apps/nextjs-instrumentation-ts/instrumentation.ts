4b751a63d645a8016d82bcdbb602dc5b67189c34
Jakub Riedl <email@jakubriedl.cz>
2025-05-13 20:01:11 +1000

    add middleware to nextjs-instrumentation test

--- apps/nextjs-instrumentation-ts/instrumentation.ts
+++ apps/nextjs-instrumentation-ts/instrumentation.ts
@@ -1,10 +1,12 @@
-import { registerAppear } from "@appear.sh/introspector/node"
 
-export const register = () => {
-  registerAppear({
-    apiKey: "test-key",
-    environment: "test",
-    serviceName: "nextjs-instrumentation-ts-test",
-    reporting: { endpoint: process.env.COLLECTOR_URL },
-  })
+export const register = async () => {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    const { registerAppear } = await import("@appear.sh/introspector/node")
+    registerAppear({
+      apiKey: "test-key",
+      environment: "test",
+      serviceName: "nextjs-instrumentation-ts-test",
+      reporting: { endpoint: process.env.COLLECTOR_URL },
+    })
+  }
 }
