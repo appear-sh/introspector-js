f376d0dfe345197c3b517765b98031e759d366c8
Jakub Riedl <email@jakubriedl.cz>
2025-09-06 21:09:09 +1000

    Add next.js preset

--- apps/nextjs/appear.js
+++ apps/nextjs/appear.js
@@ -1,17 +1,8 @@
-import { registerAppear } from "@appear.sh/introspector/node"
-import { defaultInterceptFilter } from "@appear.sh/introspector"
+import { registerAppear } from "@appear.sh/introspector/nextjs"
 
 registerAppear({
   apiKey: "test-key",
   environment: "test",
   serviceName: "nextjs-test",
   reporting: { endpoint: process.env.COLLECTOR_URL },
-  interception: {
-    filter: (request, response, config) => {
-      return (
-        !request.url.includes("/_next/") &&
-        defaultInterceptFilter(request, response, config)
-      )
-    },
-  },
 })
